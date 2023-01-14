bfb2a86fa7d3e818703ce428604a4c040c4ec756
wopian <wopian@wopian.me>
2023-01-14 07:11:48 +0000

    feat: use official sign in with steam image button

--- src/components/SteamOpenIdLogin.vue
+++ src/components/SteamOpenIdLogin.vue
@@ -24,6 +24,20 @@
     <input type="hidden" name="openid.mode" value="checkid_setup" />
     <input type="hidden" name="openid.realm" :value="realm" />
     <input type="hidden" name="openid.return_to" :value="returnTo" />
-    <button type="submit">Login with Steam</button>
+    <button type="submit">
+      <img src="@/assets/login-with-steam.png" alt="Login with Steam" />
+    </button>
   </form>
 </template>
+
+<style scoped lang="less">
+  form button {
+    border: none;
+    background: none;
+    display: flex;
+
+    img {
+      cursor: pointer;
+    }
+  }
+</style>
