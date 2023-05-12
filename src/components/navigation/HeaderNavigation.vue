6343d6b06fb3c64e67c9c8c38d105d426e4a1067
wopian <wopian@wopian.me>
2023-05-12 22:28:48 +0100

    chore: minor changes

--- src/components/navigation/HeaderNavigation.vue
+++ src/components/navigation/HeaderNavigation.vue
@@ -13,13 +13,15 @@
 
   const authStore = useAuthenticationStore()
 
-  const refreshExpiresAt = new Date(authStore.RefreshExpiry)
-  const accessExpiresAt = new Date(authStore.AccessExpiry)
+  if (authStore.RefreshExpiry && authStore.AccessExpiry) {
+    const refreshExpiresAt = new Date(authStore.RefreshExpiry)
+    const accessExpiresAt = new Date(authStore.AccessExpiry)
 
-  if (isBefore(refreshExpiresAt, Date.now())) {
-    authStore.logout()
-  } else if (isBefore(accessExpiresAt, Date.now())) {
-    authStore.refresh()
+    if (isBefore(refreshExpiresAt, Date.now())) {
+      authStore.logout()
+    } else if (isBefore(accessExpiresAt, Date.now())) {
+      authStore.refresh()
+    }
   }
 
   const menuItems: MenuItem[] = [
--- src/stores/authentication.ts
+++ src/stores/authentication.ts
@@ -5,10 +5,19 @@ import { refresh } from '~/services/gtr'
 import { getStorage, removeStorage, setStorage } from '~/utils'
 
 const id = 'authentication'
+const cache = getStorage<Authentication>(id)
 
 export const useAuthenticationStore = defineStore({
   id,
-  state: (): Authentication => ({ ...getStorage<Authentication>(id) }),
+  state: (): Authentication =>
+    cache || {
+      UserId: undefined,
+      SteamId: undefined,
+      AccessToken: undefined,
+      AccessExpiry: undefined,
+      RefreshToken: undefined,
+      RefreshExpiry: undefined
+    },
   actions: {
     login(auth: Authentication) {
       this.UserId = auth.UserId
@@ -30,12 +39,15 @@ export const useAuthenticationStore = defineStore({
     },
     async refresh() {
       console.debug('Refreshing token')
-      if (new Date(this.RefreshExpiry) < new Date()) {
+      if (this.RefreshExpiry && new Date(this.RefreshExpiry) < new Date()) {
         console.debug('Refresh token expired, logging out')
         this.logout()
-      } else {
+      } else if (this.RefreshToken && this.SteamId) {
         console.debug('Refresh token valid, refreshing')
         this.login(await refresh(this.SteamId, this.RefreshToken))
+      } else {
+        console.debug('No refresh token, logging out')
+        this.logout()
       }
     }
   }
